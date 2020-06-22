8ab3068794801947348797f7ed6cda37ddcc4bca
Abdulmateen Tairu <tay2druh@gmail.com>
2020-06-22 02:29:06 +0100

    Used Custom Admin manager for custom user

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    pass
+
+admin.site.register(User, CustomUserAdmin)
