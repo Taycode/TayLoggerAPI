9b65805b923c05165e52846acffc8605d6cfa5ed
Abdulmateen Tairu <tay2druh@gmail.com>
2020-06-22 02:18:12 +0100

    added user to log group model and created migrations for Log and log group models

--- logger/models.py
+++ logger/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class LogGroup(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
 
 
