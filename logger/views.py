08b4864c4efcf5f523df8b4e040357699e8bb0e7
Abdulmateen Tairu <tay2druh@gmail.com>
2020-06-22 02:06:49 +0100

    create log group view made

--- logger/views.py
+++ logger/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from rest_framework import status
+from .serializers import LogGroupSerializer
+
+
+class CreateLogGroup(APIView):
+
+    serializer_class = LogGroupSerializer
+
+    @staticmethod
+    def post(request):
+        serializer = LogGroupSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
