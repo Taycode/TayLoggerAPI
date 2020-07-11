aed5573916ec1074efa0d5a16eee646d75b77358
Abdulmateen Tairu <tay2druh@gmail.com>
2020-07-11 02:05:18 +0100

    preparing for deployment

--- TayLoggerAPI/settings.py
+++ TayLoggerAPI/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -142,3 +143,4 @@ CORS_ORIGIN_ALLOW_ALL = True
 AUTH_USER_MODEL = 'account.User'
 
 
+django_heroku.settings(locals())
